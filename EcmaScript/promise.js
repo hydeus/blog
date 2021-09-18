8bae9efa960a2dc5b2faf95822e5d75447650d46
sumo <wuyfnet@gmail.com>
2021-09-18 11:16:53 +0800

    update

--- EcmaScript/promise.js
+++ EcmaScript/promise.js
@@ -1,1 +1,29 @@
 // promise promise.all promise.allSettled promise.race promise.spread
+
+// 模拟promise.all
+Promise.myAll = function(promises) {
+  let result = [];
+  let promiseCount = 0;
+  let promisesLength = promises.length;
+
+  return new Promise((resolve, reject) => {
+    for (let promiseItem of promises) {
+      Promise.resolve(promiseItem).then(res => {
+        result.push(res);
+        promiseCount++;
+        if (promiseCount === promisesLength) {
+          resolve(result);
+        }
+      }, err => {
+        reject(err);
+      })
+    }
+  })
+}
+
+var p1 = new Promise((resolve) => { setTimeout(() => { return resolve(3) }, 1000) });
+var p2 = Promise.resolve(1);
+var p3 = Promise.resolve(2);
+var p = Promise.myAll([p1, p2, p3]);
+p.then(res => console.log(res)).catch(err => console.log(err));
+
---
+++ 开发工具/vscode.md
@@ -0,0 +1,5 @@
+## VS Code 快捷键
+
+1. 全部展开 ctrl+k ctrl+j
+2. 二级折叠（方法折叠） ctrl+k ctrl + 2
+3. ctrl+shift+p 查看所有快捷键命令
